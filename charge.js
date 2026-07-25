6eea0501ef12fe58692014a8d10cbe666ccc1ee4
Aliou Sall <zetic.conseils@gmail.com>
2026-07-25 15:38:07 +0000

    Mise à jour du projet

---
+++ charge.js
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const API_KEY = process.env.OPENROUTER_API_KEY;
---
+++ use.js
@@ -0,0 +1,6 @@
+const API_KEY = process.env.OPENROUTER_API_KEY;
+
+const headers = {
+    Authorization: `Bearer ${API_KEY}`,
+    "Content-Type": "application/json"
+}
