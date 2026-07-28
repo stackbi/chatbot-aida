fe27daab2ab7bc6269e625045fa87a1616699599
Aliou Sall <zetic.conseils@gmail.com>
2026-07-28 13:58:48 +0000

    Maj secu

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use(helmet({
   contentSecurityPolicy: {
     directives: {
       defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
       styleSrc: ["'self'", "'unsafe-inline'"],
       imgSrc: ["'self'", "data:", "blob:"],
       connectSrc: ["'self'"],
